5204174d67337e26a7256c3eac3cf9bf78dbed38
Manuel Sidler <sidler.manuel@gmail.com>
2019-03-16 12:19:20 +0100

    do some cleanup (remove unnecessary files etc.)

--- src/RazorPagesSample.Web/Pages/BookDetail.cshtml.cs
+++ src/RazorPagesSample.Web/Pages/BookDetail.cshtml.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesSample.Web.Models;
 using RazorPagesSample.Web.Services;
